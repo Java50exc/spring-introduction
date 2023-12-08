25ba7fb3c9db3d17c48949981e0fe984dd861e82
Yuragranovsky <yuragranovsky@gmail.com>
2023-12-08 12:22:15 +0200

    CW #52 Spring Introduction (Calculator)

---
+++ src/main/java/telran/spring/controller/CalculatorController.java
@@ -0,0 +1,12 @@
+package telran.spring.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("calculator")
+public class CalculatorController {
+	@GetMapping("add")
+	double add(double op1, double op2) {
+		return op1 + op2;
+	}
+}
